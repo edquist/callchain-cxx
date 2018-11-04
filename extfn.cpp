18234aa492f13e4c243c57554d6ae4d486f05b4f
Carl Edquist <edquist@cs.wisc.edu>
2018-11-03 20:14:12 -0500

    something aweful...
    but it works

--- extfn.cpp
+++ extfn.cpp
@@ -14,7 +14,7 @@ struct CallchainV;
 
 template <class V>
 CallchainV<V>
-callchain(const V &v)
+callchain(V v)
 {
 	return CallchainV<V>(v);
 }
@@ -23,12 +23,9 @@ template <class R, class V>
 struct CallchainFV1 {
 	typedef R  fn_t(V);
 	fn_t      *fn;
-	const V   &val;
+	V          val;
 
-	CallchainFV1(fn_t *fn_, const V &val_) : fn(fn_), val(val_)
-	{
-		std::cout << "[CallchainFV1(" << val << ")]\n";
-	}
+	CallchainFV1(fn_t *fn_, V val_) : fn(fn_), val(val_) {}
 
 	CallchainV<R>
 	operator() () const
@@ -39,15 +36,12 @@ template <class R, class V, class A2>
 struct CallchainFV2 {
 	typedef R  fn_t(V, A2);
 	fn_t      *fn;
-	const V   &val;
+	V          val;
 
-	CallchainFV2(fn_t *fn_, const V &val_) : fn(fn_), val(val_)
-	{
-		std::cout << "[CallchainFV2(" << val << ")]\n";
-	}
+	CallchainFV2(fn_t *fn_, V val_) : fn(fn_), val(val_) {}
 
 	CallchainV<R>
-	operator() (const A2 &a2) const
+	operator() (A2 a2) const
 	{ return CallchainV<R>(fn(val, a2)); }
 };
 
@@ -55,26 +49,20 @@ template <class R, class V, class A2, class A3>
 struct CallchainFV3 {
 	typedef R  fn_t(V, A2, A3);
 	fn_t      *fn;
-	const V   &val;
+	V          val;
 
-	CallchainFV3(fn_t *fn_, const V &val_) : fn(fn_), val(val_)
-	{
-		std::cout << "[CallchainFV3(" << val << ")]\n";
-	}
+	CallchainFV3(fn_t *fn_, V val_) : fn(fn_), val(val_) {}
 
 	CallchainV<R>
-	operator() (const A2 &a2, const A3 &a3) const
+	operator() (A2 a2, A3 a3) const
 	{ return CallchainV<R>(fn(val, a2, a3)); }
 };
 
 template <class V>
 struct CallchainV {
-	const V &v;
+	V v;
 
-	CallchainV(const V &v_) : v(v_)
-	{
-		std::cout << "[CallchainV(" << v << ")]\n";
-	}
+	CallchainV(V v_) : v(v_) {}
 
 	template <class R>
 	CallchainFV1<R,V>
@@ -84,32 +72,22 @@ struct CallchainV {
 	template <class R, class A2>
 	CallchainFV2<R,V,A2>
 	operator() (R (*f)(V,A2))
-	{
-		std::cerr << "[CallchainV (FV2) v=" << v << "]\n";
-		return CallchainFV2<R,V,A2>(f,v); }
+	{ return CallchainFV2<R,V,A2>(f,v); }
 
 	template <class R, class A2, class A3>
 	CallchainFV3<R,V,A2,A3>
 	operator() (R (*f)(V,A2,A3))
 	{ return CallchainFV3<R,V,A2,A3>(f,v); }
 
-	const V &operator() () const
-	{
-		return v;
-	}
+	V operator() () const
+	{ return v; }
 };
 
-int foo(int x) {
-	std::cerr << "[hi from foo(" << x << ")]\n";
-	return x * 10000; }
+int foo(int x) { return x * 10000; }
 
-int bar(int x, int y) {
-	std::cerr << "[hi from bar(" << x << "," << y << ")]\n";
-	return x + 100 * y; }
+int bar(int x, int y) { return x + 100 * y; }
 
-int baz(int x, int y) {
-	std::cerr << "[hi from baz(" << x << "," << y << ")]\n";
-	return x + y; }
+int baz(int x, int y) { return x + y; }
 
 int main()
 {
@@ -118,18 +96,17 @@ int main()
 		("Mr")
 		("Man");
 
-	int nn = callchain(99)
-		(foo)()
-		(bar)(88)
-		(baz)(77)
-		();
+	std::cout << "\n\n";
+
+	int nn = callchain(99)(foo)()
+		              (bar)(88)
+		              (baz)(77)
+		              ();
+
 	std::cout << nn << "\n\n";
+
 	std::cout << baz(bar(foo(99), 88), 77) << "\n";
 }
 
-#if 0
-	foo(99).bar(88).baz(77) -> baz(bar(foo(99), 88), 77)
-
-	foo(99) ? (bar)(88)
-#endif
+//	foo(99).bar(88).baz(77) -> baz(bar(foo(99), 88), 77)
 
