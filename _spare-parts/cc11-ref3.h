d1b06e64316578d01d886d7aeb38635cfc2acb5a
Carl Edquist <edquist@cs.wisc.edu>
2019-09-18 08:52:47 -0500

    another doesn't-work ref version
    basically identical to cc11-ref2.h, but terser.
    
    still doesn't work for the same reason, that rvalue refs become invalid
    too early when the objects go out of scope.

---
+++ _spare-parts/cc11-ref3.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <utility>
+
+template <class T> struct Callchain;
+
+template <class T>
+inline Callchain<T &&> callchain(T &&x)
+{
+    return Callchain<T &&> (std::forward<T>(x));
+}
+
+template <class T>
+struct Callchain {
+    T value;
+
+    Callchain(T &&x) : value(std::forward<T>(x)) {}
+
+    template <class F, class ...Args>
+    auto operator() (F &f, Args &&...args)
+      -> decltype(callchain(f(std::forward<T>(value),
+                              std::forward<Args>(args)...)))
+    {
+        return callchain(f(std::forward<T>(value),
+                           std::forward<Args>(args)...));
+    }
+
+    T &&operator() ()
+    { return std::forward<T>(value); }
+};
+
+// callchain(x)(foo)(bar,y,z)(baz)() --> baz(bar(foo(x),y,z))
+
